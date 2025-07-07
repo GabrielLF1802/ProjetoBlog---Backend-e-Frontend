37c19fd02a6a7ef043d1dc937b37ae701ec8d86b
Gabriel <gabriellourencofaria@gmail.com>
2025-07-07 18:54:00 -0300

    função de edit e delet de categorias

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ const Categoria = mongoose.model('categorias')
 router.get('/',(req,res)=>{
     res.render('admin/index')
 })
+
 router.get('/categorias',(req,res)=>{
   Categoria.find().then((categorias)=>{
     res.render('admin/categorias', {categorias: categorias})
@@ -51,5 +52,44 @@ router.post('/categorias/nova', (req, res) => {
 
 })
 
+router.get('/categorias/edit/:id', (req,res)=>{
+  Categoria.findOne({_id:req.params.id}).then((categoria)=>{
+    res.render("admin/editcategoria",{categoria:categoria})
+  }).catch((err)=>{
+    req.flash('error_msg', 'Essa categoria não existe')
+    res.redirect('/admin/categorias')
+  })
+  
+
+})
+
+router.post('/categorias/edit',(req,res)=>{
+  Categoria.findOne({_id:req.body.id}).then((categoria)=>{
+    categoria.nome= req.body.nome
+    categoria.slug= req.body.slug
+
+    categoria.save().then(()=>{
+      req.flash('sucess_msg','Categoria editada com sucesso!')
+      res.redirect('/admin/categorias')
+    }).catch((err)=>{
+      req.flash('error_msg','Erro ao salvar a categoria')
+      res.redirect('/admin/categorias')
+    })
+
+  }).catch(()=>{
+    req.flash('error_msg','Houve um erro ao editar a categoria')
+    res.redirect('/admin/categorias')
+  })
+})
+
+router.post('/categorias/delet',(req,res)=>{   
+   Categoria.deleteOne({id:req.body.id}).then(()=>{
+    req.flash('sucess_msg','Categoria deletada com sucesso!')
+    res.redirect('/admin/categorias')
+  }).catch((err)=>{
+    req.flash('error_msg','Não foi possível deletar a categoria')
+    res.redirect('/admin/categorias')
+  })
+})
 
 module.exports = router
