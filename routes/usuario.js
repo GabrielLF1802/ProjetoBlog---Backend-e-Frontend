4a49786f263c72ed1828aa6b52ed0a55977824c6
Gabriel <gabriellourencofaria@gmail.com>
2025-07-09 15:40:26 -0300

    Verificação de existencia de usuário no cadastro

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router= express.Router()
 const mongoose= require('mongoose')
 require('../models/Usuario')
 const Usuario= mongoose.model('usuarios')
+const bcrypt= require('bcryptjs')
 
 router.get('/registro',(req,res)=>{
     res.render('usuario/registro')
@@ -28,9 +29,44 @@ router.post('/registro', (req,res)=>{
         res.render('usuario/registro',{erros:erros})
 
     }else{
+        Usuario.findOne({email:req.body.email}).then((usuario)=>{
+            if(usuario){
+                req.flash('error_msg','Email já registrado')
+                res.redirect('/usuario/registro')
+            }else{
+                const novoUsuario = new Usuario({
+                    nome:req.body.nome,
+                    senha:req.body.senha,
+                    email:req.body.email
+                })
+                bcrypt.genSalt(10, (erro, salt)=>{
+                    bcrypt.hash(novoUsuario.senha, salt, (erro,hash)=>{
+                        if(erro){
+                            req.flash('error_msg','Erro no sistema de cadastro')
+                            res.redirect('/')
+                        }
+                        novoUsuario.senha = hash
+                        novoUsuario.save().then(()=>{
+                            req.flash('sucess_msg','Novo usuário cadastrado com sucesso!')
+                            res.redirect('/')
+                        }).catch((err)=>{
+                            req.flash('error_msg','Falha ao cadastrar novo usuário')
+                            res.redirect('/')
+                        })
+                    })
+                })
+                
+            }
+        }).catch((err)=>{
+            req.flash('error_msg','Erro no registro')
+            res.redirect('/')
+        })
 
     }
 
+})
+router.get('/login',(req,res)=>{
+    res.render('usuario/login')
 })
 
 
